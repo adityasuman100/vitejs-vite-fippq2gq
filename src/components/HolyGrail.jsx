9962c2d4dcf143d912b935d62992d95995320e02
adityasuman100 <adityasuman100@gmail.com>
2025-01-15 09:50:53 +0530

    HolyGrail

---
+++ src/components/HolyGrail.jsx
@@ -0,0 +1,16 @@
+import './HolyGrail.css';
+
+export default function App() {
+    return (
+      <>
+        <header>Header</header>
+        <div className="columns">
+          <nav>Navigation</nav>
+          <main>Main</main>
+          <aside>Sidebar</aside>
+        </div>
+        <footer>Footer</footer>
+      </>
+    );
+  }
+  
